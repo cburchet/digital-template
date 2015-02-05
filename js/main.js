d5596368abe65501283efb9187227121d2e97420
cburchet <cburchet@gmu.edu>
2015-02-05 00:32:11 -0500

    add var cursors

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ window.onload = function()
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 	var player;
+	var cursors;
     var earth;
 	var rocks;
 	var dogs;
